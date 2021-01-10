3a58fcd0c5c512cdd51c93b465e3ede0e6ddd1c4
visky39 <77222249+visky39@users.noreply.github.com>
2021-01-10 23:55:33 +0700

    Add files via upload

---
+++ Homework1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	cout<<" 1st Term of 2020        "<<"6206021611141 Anantapon phonsamud"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"------------------------------------------------------------------------------"<<endl;
+	cout<<"| Subject ID | Subject name                        |"<<"Score(100) | Grade |"<<endl;
+	cout<<"| 060243107  | COMPUTER ARCHITECTURE               |"<<"    62     |  'C'  |"<<endl;
+	cout<<"| 060243108  | DATABASE SYSTEM                     |"<<"    65     | 'C+'  |"<<endl;
+	cout<<"| 060243109  | COMPUTER NETWORK AND COMMUNICATION  |"<<"    72     |  'B'  |"<<endl;
+	cout<<"| 080303608  | CITIZENSHIP AND SOCIAL RESPONATION  |"<<"    81     |  'A'  |"<<endl;
+	cout<<"------------------------------------------------------------------------------"<<endl;
+	cout<<"|    TOTAL   | Subject = 4                         |"<<"Average = 2.87     |"<<endl;
+	cout<<"=============================================================="<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"KMUTNB"<<endl;
+	
+	return(0);
+}
